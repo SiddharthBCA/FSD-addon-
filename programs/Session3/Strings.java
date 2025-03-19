88af2da764ef83c504441550c6edfc32750c6329
Siddharthsid <141048050+SiddharthBCA@users.noreply.github.com>
2025-03-19 15:18:58 +0530

    day3

---
+++ programs/Session3/Strings.java
@@ -0,0 +1,17 @@
+package Session3;
+
+public class Strings {
+	
+	public static void main(String[] args) {
+		String s1 = "Siddharth";
+		String s2 = new String("Sid sir");//String objects
+		char[] c1 = {'J', 'A', 'V', 'A'};// Array of characters
+		String s3 = new String(c1);//char to String
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+	}
+
+}
